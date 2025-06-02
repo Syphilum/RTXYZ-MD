408cb680b516fe585fd6bd8b273da72c45040679
Syphilum <syphilum@gmail.com>
2025-06-03 00:01:24 +0800

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -16,7 +16,7 @@ let path = require('path')
 let fetch = require('node-fetch')
 let moment = require('moment-timezone')
 let levelling = require('../lib/levelling')
-let arrayMenu = ['all', 'main', 'downloader', 'rpg', 'rpgG', 'sticker', 'advanced', 'xp', 'fun', 'game', 'github', 'group', 'image', 'nsfw', 'info', 'internet', 'islam', 'kerang', 'maker', 'owner', 'voice', 'quotes', 'store', 'stalk', 'shortlink', 'tools', 'anonymous', ''];
+let arrayMenu = ['all', 'main', 'downloader', 'rpg', 'rpgG', 'sticker', 'advanced', 'xp', 'fun', 'game', 'github', 'group', 'image', 'nsfw', 'info', 'internet', 'islam', 'kerang', 'maker', 'owner', 'voice', 'quotes', 'store', 'stalk', 'shortlink', 'tools', 'anonymous', 'elfie', ''];
 
 
 const allTags = {
@@ -47,6 +47,7 @@ const allTags = {
     'shortlink': 'SHORT LINK',
     'tools': 'MENU TOOLS',
     'anonymous': 'ANONYMOUS CHAT',
+    'elfie': 'JIKOSHOKAI ELFIE',
     '': 'NO CATEGORY'
 }
 
